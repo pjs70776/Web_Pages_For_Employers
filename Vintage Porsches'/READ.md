5d0a1efc0cd40a16e752571ece39cb356aa1cdd4
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-07-15 10:45:32 -0700

    update

---
+++ Vintage Porsches'/READ.md
@@ -0,0 +1,16 @@
+# Live Project
+
+# Introduction
+For the last two weeks of my time at The Tech Academy, I worked with my peers in an Agile/Scrum environment.  The goal was to develop a fully functional webpage about a product that was of interest; I chose a classic car dealership.  The languages I used were HTML, CSS, and JavaScript, with Bootstrap used to improve my time management and overall design ideas.  Working on a team and getting practical experience in a simulated work environment was extremely rewarding to see and be a part of.
+
+# HTML
+My HTML portion included a Navbar, and sections such as: Home, About Us (which included an image carousel), and a Gallery (for vehicles currently available for sale).  In addition, I included a 'return to top' button for ease of navigation, as well as a functional Subscribe/Contact pop out box.  Below, you will find a sample of my code that has created the button trigger modal and the components required to create the Subscirbe box.  
+![html1](https://github.com/pjs70776/HTML-and-CSS-Projects/assets/169723114/bb68b26c-4a36-4866-b3ba-04e4c45d15cc)
+
+# CSS
+My CSS section includes all of the basic styling elements for generic styles for font (h1, h2, etc.), the home section layout, container/gallery styles, as well as the contact form syles. What you are seeing below is my code written to stylize the contact form. 
+![css1 jog](https://github.com/pjs70776/HTML-and-CSS-Projects/assets/169723114/341a1578-7bd0-40f2-ba2d-02d1b91bfbaf)
+
+# JavaScript
+In my JavaScript section, I made the webpage even more sytlized.  Keeping the current year updated in the footer was one element.  Giving the modal element (the return to top button) fucnitonality, such as going to the top of the document was also added.  Lastly, allowing the viewer to toggle a dark mode for the page was inserted. In this image, you can see the modal and dark mode elements. 
+![js1](https://github.com/pjs70776/HTML-and-CSS-Projects/assets/169723114/3d0dcaf1-0049-4f5d-98f7-62c0eba51d9b)
---
+++ Vintage Porsches'/static/scripts.js
@@ -0,0 +1,84 @@
+// Create a new Date object
+const currentDate = new Date();
+
+// Get the current year from the date object
+const currentYear = currentDate.getFullYear();
+
+// Example usage: appending the current year to a string
+const message = `${currentYear}`;
+document.getElementById("currentYear").innerHTML = currentYear;
+
+//grid
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// Full-width images
+function one() {
+    for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";  // IE10
+    elements[i].style.flex = "100%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = document.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+//  modal
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+//  button to return to top of page
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// dark mode
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
